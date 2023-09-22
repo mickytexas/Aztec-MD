cc24a42cc0de6501a2fca62741f2d0c5b1965f98
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-22 16:35:20 +0200

    config

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
+require('dotenv').config()
 
+module.exports.config = () => {
+    return {
+        botName: process.env.BOTNAME || 'AZTEC MD',
+        prefix: process.env.PREFIX || '.',
+        mongodb: process.env.MONGODB || '',
+        port: process.env.PORT || '8000',
+        caption: process.env.CAPTION || 'By Aztec MD',
+        mods: (process.env.MODS || '').split(',')
+        }
+       }
