fd762c9e68a1dcdc5a2c697b684fe5755ae9ae9f
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-22 16:45:01 +0200

    vorterx

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports.config = () => {
     return {
         botName: process.env.BOTNAME || 'AZTEC MD',
         prefix: process.env.PREFIX || '.',
+        session: process.env.SESSION || 'add something', //replace this to any text
         mongodb: process.env.MONGODB || '',
         port: process.env.PORT || '8000',
         caption: process.env.CAPTION || 'By Aztec MD',
