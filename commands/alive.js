66925a2e0b9663340ea03bdac1592a32de9fbde9
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-22 18:41:37 +0200

    Create alive.js

---
+++ commands/alive.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'alive',
+  category: 'alive',
+  description: 'alive',
+  start: async(vorterx,m,args) {
+
+   let nxn = ` HI THERE LM ALIVE REALLY ALIVE`;
+   vorterx.sendMessage(m.from,{ caption: nxn}, {quoted: m}
+ } 
+   
