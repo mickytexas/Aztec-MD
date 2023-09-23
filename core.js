4e9a62273f6a95d8c138e486362b39bb1e1763cd
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-23 15:30:06 +0200

    vorterx

--- core.js
+++ core.js
@@ -117,7 +117,6 @@ console.log(chalk.green.bold("👨‍💻You have connected to Aztec-MD"));
     vorterx.ev.on('messages.upsert', async (messages) => await MessageHandler(messages, vorterx))
    vorterx.ev.on('contacts.update', async (update) => await contact.saveContacts(update, vorterx))
    }
-    }
           
    startAztec()
   .catch((err) => console.error(err))
