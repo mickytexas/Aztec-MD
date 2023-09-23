944ec0add9bbd584eb161ee6cd00cfe018012686
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-23 07:57:48 +0200

    vorterx

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
 
-module.exports.config = () => {
-    return {
-        botName: process.env.BOTNAME || 'AZTEC MD',
-        prefix: process.env.PREFIX || '.',
-        session: process.env.SESSION || 'add something', //replace this to any text
-        mongodb: process.env.MONGODB || '',
-        port: process.env.PORT || '8000',
-        caption: process.env.CAPTION || 'By Aztec MD',
-        mods: (process.env.MODS || '').split(',')
+        global.botName = process.env.BOTNAME || 'AZTEC MD',
+        global.prefix = process.env.PREFIX || '.',
+        global.session = process.env.SESSION || 'add something', //replace this to any text
+        global.mongodb = process.env.MONGODB || '',
+        global.port = process.env.PORT || '8000',
+        global.caption = process.env.CAPTION || 'By Aztec MD',
+        global.mods = (process.env.MODS || '').split(',')
         }
-       }
+module.exports = {
+mongodb: global.mongodb,
+  };
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update'${__filename}'`));
+  delete require.cache[file];
+  require(file);
+});
