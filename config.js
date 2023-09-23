cb7ea00e5acbad4be66ea156b583df357d622878
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-23 16:08:12 +0200

    vorterx

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const chalk = require('chalk');
         global.botName = process.env.BOTNAME || 'AZTEC MD',
         global.prefix = process.env.PREFIX || '.',
         global.session = process.env.SESSION || 'add something', //replace this to any text
-        global.mongodb = process.env.MONGODB || '',
+        global.mongodb = process.env.MONGODB || 'mongodb+srv://diegoson:fenandes@cluster0.7vgtwsd.mongodb.net/?retryWrites=true&w=majority',
         global.port = process.env.PORT || '8000',
         global.caption = process.env.CAPTION || 'By Aztec MD',
         global.mods = (process.env.MODS || '').split(',')
