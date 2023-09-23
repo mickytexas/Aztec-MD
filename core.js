7bd9a0c985f40b4ac5088a6485e87172944c12c8
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-23 11:13:36 +0200

    connect

--- core.js
+++ core.js
@@ -16,6 +16,7 @@ const {
 const { Boom } = require('@hapi/boom');
 const P = require('pino');
 const express = require("express");
+const config = require('./config');
 const vorterx= require('./lib/message/vorterx.js');
 const { QuickDB } = require('quick.db')
 const { MongoDriver } = require('quickmongo');
