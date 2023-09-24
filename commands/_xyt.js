a452b59e24def055d66a1fab5ecd44126c6a5f97
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-24 17:23:39 +0200

    vorterx

---
+++ commands/_xyt.js
@@ -0,0 +1,35 @@
+//================================>
+
+//  AZTEC MD V3.0.0
+
+//================================>
+const {fetchBuffer} = require("../mangoes/myFunc.js");
+const yts = require("youtube-yts");
+
+module.exports = {
+   name: "xyt",
+   description: "To search for music link",
+   category: "Download",
+   async xstart(vorterx,m,{xReact,text,args})  {
+
+if (!args) { await xReact("⛔"); return m.reply("*Please give a term example  yts hope*");
+           }
+    await xReact("🌛");
+let search = await yts(text)
+url = search.videos[0].url
+let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+diego = await fetchBuffer(anu.thumbnail)
+let aztec = `*╭────❰*\n
+*❒* *Title* : *${anu.title}*\n
+*❒* *ID* : *${anu.videoId}*\n
+*❒* *Viewers* : *${anu.views}*\n
+*❒* *Upload At* : *${anu.ago}*\n
+*❒* *Author* : *${anu.author.name}*\n
+*❒* *Link* : ${anu.url}\n
+*╰─────────────⭓*\n\n*©vorterx* 
+`
+vorterx.sendMessage(m.from, { image : diego, caption: aztec }, { quoted: m})
+   }
+};
+
+//================================>
