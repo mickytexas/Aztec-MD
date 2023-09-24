1d8de4ef7702d88be8712afaabb2d97bb3bd8421
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-24 15:17:44 +0200

    vort

--- mangoes/myFunc.js
+++ mangoes/myFunc.js
@@ -3,6 +3,18 @@ const axios = require("axios");
 exports.getRandom = (Numb) => {
 return `${Math.floor(Math.random() * 10000)}${Numb}`
 }
+exports.runtime = function(seconds) {
+	seconds = Number(seconds);
+	var d = Math.floor(seconds / (3600 * 24));
+	var h = Math.floor(seconds % (3600 * 24) / 3600);
+	var m = Math.floor(seconds % 3600 / 60);
+	var s = Math.floor(seconds % 60);
+	var dDisplay = d > 0 ? d + (d == 1 ? " d, " : " d, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? " h, " : " h, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? " m, " : " m, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? " s" : " s") : "";
+	return dDisplay + hDisplay + mDisplay + sDisplay;
+}
 
 exports.getBuffer = async (url) => {
   try {
