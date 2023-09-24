04df11c928dd8aaec40127e2c597aac85c61a04e
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-24 09:48:20 +0200

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+const axios = require ('axios');
+
+module.exports = {
+  name: 'ping',
+  desc: 'yes',
+  category: 'ccc',
+  xstart: async(vorterx,m,{xReact}) => {
+
+    var inital = new Date().getTime();
+    const { key } = await vorterx.sendMessage(m.from, {text: '_Checking ping!!!_'});
+    var final = new Date().getTime();
+   // await Secktor.sleep(1000)
+   return await vorterx.sendMessage(m.from, {text: '*𝘗𝘐𝘕𝘎*\n *' + (final - inital) + ' 𝘔𝘚* ', edit: key});
+  }
+};
