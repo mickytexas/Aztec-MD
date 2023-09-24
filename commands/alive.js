17fc79d0bfa28587c71c1fc461365fbd532944ae
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-24 15:13:22 +0200

    vorterx

--- commands/alive.js
+++ commands/alive.js
@@ -1,12 +1,30 @@
-/*const axios = require('axios');
+const fs = require("fs");
+require("../config");
 
 module.exports = {
-  name: 'alive',
-  category: 'alive',
-  description: 'alive',
-  start: async(vorterx,m,args) {
-
-   let nxn = ` HI THERE LM ALIVE REALLY ALIVE`;
-   vorterx.sendMessage(m.from,{ caption: nxn}, {quoted: m}
- } 
-   */
+   name: 'alive',
+   category: 'General',
+   description: 'An cmd to test if its on',
+   async xstart(vorterx, m, {m.pushName, xReact }) => {
+      await toReact("💗");
+   //let aztec = fs.readFileSync("./lib/connect/aztec.png");
+     // let anexa = fs.readFileSync("./lib/connect/anexa.png");
+      let msg = `*Hey ${m.pushName} This is ${process.env.BOTNAME} developed by Diegoson*\n\n*👾Descripto*: WhatsApp user bot\n\n*👾Prefix*: ${process.env.Prefix}\n\n*📲Version*: 3.0.0\n\n\nType ${process.env.Prefix}menu to get my full command list`;
+      let Amarok = {
+         image: aztec,
+         caption: msg,
+         contextInfo: {
+              externalAdReply: {
+                 title: `vorterx team`,
+                 body: 'ʙᴇsᴛ ᴛᴏ ᴜsᴇ',
+                 thumbnail: anexa,
+                 mediaType: 1,
+                 mediaUrl: ``,
+                 sourceUrl: `vorterx.com/`,
+                 ShowAdAttribution: true,
+              },
+         },
+      };
+     await vorterx.sendMessage(m.from, Amarok, { quoted: m})
+                                              }
+};
