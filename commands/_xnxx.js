5ebf54625b28f37a7e4b24ee4cf227695ac31084
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-25 10:24:15 +0200

    vorterx;:

---
+++ commands/_xnxx.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const fs = require("fs");
+
+module.exports = {
+  name: "xnxxsh",
+  description: "18 videos only",
+  category: "Download",
+  async xstart(vorterx,m,{xReact, text,args}) => {
+
+  let me = fs.readFileSync("./lib/inogs.jpg");
+  if(!text) {
+    await xReact("⛔");
+    return m.reply("Giv me a search term please*");
+  }
+    await xReact("👽");
+
+  var res = (await axios(`https://raganork-network.vercel.app/api/xvideos/search?query=${text}`)).data
+
+  let textt = "*XNXXV SEARCH*\n\nRESULTS FROM " + text + "\n\n";
+
+ let no = 1;
+  for (var video of res.result) {
+    textt += `\n*╭────❰*\n *❒No* : ${no++}\n*❒Title* : ${video.title}\n*❒Duration* : ${video.duration}\n*❒Url* : ${video.url}\n*╰─────────────*\n\n`;
+
+  }
+
+  return vorterx.sendMessage(m.from, {
+    image: me, 
+    caption: textt
+  }, {
+    quoted: m
+  });
+}
+    };
