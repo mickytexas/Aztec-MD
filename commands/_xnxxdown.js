da6ad3d75c99a39531f79066330410af8b6b7ce4
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-27 20:45:58 +0200

    vorterx l

--- commands/_xnxxdown.js
+++ commands/_xnxxdown.js
@@ -1,40 +1,59 @@
+//=======
+//==DN DOWNLOAD 
+
+//=======AZTEC-MD
+
 module.exports = {
   name: "xnxxdn",
   description: "To download xnxx videos",
   category: "Download",
-  async xstart (vorterx,m,{xReact, text,args})  {
+  async xstart(vorterx, m, { xReact, text, args }) {
+    const axios = require("axios");
 
-  const axios = require("axios");
+    if (!text) {
+      await xReact("⛔");
+      m.reply("*Missing XNXX link, please provide one.*");
+      return;
+    }
 
-  
-      if (!text) { await xReact("⛔");
-        m.reply(`*Missing xnxx link please provide that*`);
-        return;
-      }
-        let urlYt = text;
-        if (!urlYt.startsWith("https")) { await toReact("⛔"); return m.reply(`*Provide me an xnxxvd link*`);
-                                        }
-      await xReact("🍑");
+    let urlYt = text;
+    if (!urlYt.startsWith("https")) {
+      await toReact("⛔");
+      return m.reply("*Provide me with an XNXXVD link.*");
+    }
 
-        const res = await axios(`https://raganork-network.vercel.app/api/xvideos/download?url=${text}`);
-        const video = res.data;
+    await xReact("💦");
 
+    const res = await axios(`https://raganork-network.vercel.app/api/xvideos/download?url=${text}`);
+    const video = res.data;
 
-        let buttonMessage = {
-          video: video,
-          mimetype: 'video/mp4',
-          fileName: `vorterx.mp4`,
-          caption: `*╭────❰*\n
-*❒* *TITLE*: *XNXX*\n
-*❒* *BOTNAME*: *${process.env.BOTNAME}*
-*╰─────────────*`,
-          gifPlayback: false,
-          height: 496,
-          width: 640,
-          headerType: 4,
-          headerType: 4,
+    let template = `
+🎬 *XNXX VIDEO DOWNLOAD*
 
-        }
-        return await vorterx.sendMessage(m.from, buttonMessage, { quoted: m })
-      } 
+|   |   |
+|---|---|
+| Download Link | [${video}](${video}) |
+| Title | XNXX |
+| Bot Name | ${process.env.BOTNAME} |
+`;
+
+    let buttonMessage = {
+      video: video,
+      mimetype: "video/mp4",
+      fileName: `vorterx.mp4`,
+      caption: template,
+      gifPlayback: false,
+      height: 496,
+      width: 640,
+      headerType: 4,
+      headerType: 4,
+        messageOptions: {
+        textColor: "#ffffff", 
+        backgroundColor: "#333333", 
+        footerBackgroundColor: "#222222",  
+        },
+    };
+
+    return await vorterx.sendMessage(m.from, buttonMessage, { quoted: m });
+  },
 };
