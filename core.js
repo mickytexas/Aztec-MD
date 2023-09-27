6cd12236627f152559eadf25f13ae746f6531f88
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-27 22:19:50 +0200

    vorterx

--- core.js
+++ core.js
@@ -42,11 +42,11 @@ const store = makeInMemoryStore({
 function startAztec() {
   useMultiFileAuthState('session')
     .then(({ state, saveCreds }) => {
-console.log(
+      console.log(
   "AZTEC MD",
   "font-weight: bold;font-size: 50px;color: red;text-shadow: 3px 3px 0 rgb(217,31,38) ,6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) ,12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) ,18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); margin-bottom: 12px; padding: 5%"
 );
-      const vorterxInstance = AztecConnect({
+   const vorterxInstance = AztecConnect({
         logger: P({ level: "silent" }),
         printQRInTerminal: true,
         browser: Browsers.macOS("Desktop"),
@@ -145,11 +145,13 @@ console.log(
 
       app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "public", "index.html"));
-        
+      });
+
       app.listen(PORT, () => {
         console.log(`[🌐 AZTEC ] Web server is running on port ${PORT}`);
       });
-     })
+    })
     .catch(console.error);
 }
+
 startAztec();
