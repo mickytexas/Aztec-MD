391003365f3f41779ef0a2529c1b2533a90a1e09
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-27 16:51:11 +0200

    vorterx

--- commands/_google.js
+++ commands/_google.js
@@ -1,5 +1,5 @@
 const google = require('google-it');
-const { fancy, box, magic, rainbow } = require("@viper-x/fancytext");
+const chalk = require('chalk');
 
 module.exports = {
   name: "google",
@@ -16,18 +16,10 @@ module.exports = {
         aztec += `🌐 *Link*: ${g.link}\n\n───────────────────────\n\n`;
       }
 
-      const templates = [
-        { name: "Template 1", text: fancy(aztec, "bold") },
-        { name: "Template 2", text: fancy(aztec, "italic") },
-        { name: "Template 3", text: box(aztec) },
-        { name: "Template 4", text: magic(aztec) },
-        { name: "Template 5", text: rainbow(aztec) },
-      ];
-
-      const selectedTemplate = templates[Math.floor(Math.random() * templates.length)];
+      const formattedAztec = chalk.bold(aztec); // Formatting with chalk
 
       const img = "https://i.ibb.co/k2mkzHJ/IMG-20230723-WA0071.jpg";
-      vorterx.sendMessage(m.from, { image: { url: img }, caption: selectedTemplate.text }, { quoted: m });
+      vorterx.sendMessage(m.from, { image: { url: img }, caption: formattedAztec }, { quoted: m });
     }).catch(err => {
       console.error(err);
     });
