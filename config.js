064ede812eafc03001546058be40ef4ca0522fe2
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-27 22:22:44 +0200

    vorterx

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
         global.prefix = process.env.PREFIX || '.',
         global.session = process.env.SESSION || 'add something', //replace this to any text
         global.thumb = process.env.THUMB || 'https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg',
-        global.mongodb = process.env.MONGODB || 'mongodb+srv://amarokaztec:d3centx@vorterx.2yhhna0.mongodb.net/?retryWrites=true&w=majority',
+        global.mongodb = process.env.MONGODB || 'mongodb+srv://mk:mk@new.vxtfxii.mongodb.net/?retryWrites=true&w=majority',
         global.port = process.env.PORT || '2000',
         global.caption = process.env.CAPTION || 'By Aztec MD',
         global.mods = (process.env.MODS || '').split(',')
