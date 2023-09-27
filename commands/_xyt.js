bf72535322c6565b2f4c2f89b55e1202494760db
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-27 15:13:02 +0200

    VORTERX

--- commands/_xyt.js
+++ commands/_xyt.js
@@ -3,33 +3,46 @@
 //  AZTEC MD V3.0.0
 
 //================================>
-const {getBuffer} = require("../mangoes/myFunc.js");
+const { getBuffer } = require("../mangoes/myFunc.js");
 const yts = require("youtube-yts");
 
 module.exports = {
-   name: "xyt",
-   description: "To search for music link",
-   category: "Download",
-   async xstart(vorterx,m,{xReact,text,args})  {
+  name: "xyt",
+  description: "Search for music link",
+  category: "Download",
+  async xstart(vorterx, m, { xReact, text }) {
+    if (!text) {
+      await xReact("⛔");
+      return m.reply("Please provide a search term. Example: xyt hope");
+    }
 
-if (!text) { await xReact("⛔"); return m.reply("*Please give a term example  xyt hope*");
-           }
-    await xReact("🌛");
-let search = await yts(text)
-url = search.videos[0].url
-let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
-diego = await getBuffer(anu.thumbnail)
-let aztec = `*╭────❰*\n
-*❒* *Title* : *${anu.title}*\n
-*❒* *ID* : *${anu.videoId}*\n
-*❒* *Viewers* : *${anu.views}*\n
-*❒* *Upload At* : *${anu.ago}*\n
-*❒* *Author* : *${anu.author.name}*\n
-*❒* *Link* : ${anu.url}\n
-*╰─────────────⭓*\n\n*©vorterx* 
-`
-vorterx.sendMessage(m.from, { image : diego, caption: aztec }, { quoted: m})
-   }
-};
+    await xReact("🎵");
+    const search = await yts(text);
+    const randomVideo = search.videos[Math.floor(Math.random() * search.videos.length)];
+    const thumbnail = await getBuffer(randomVideo.thumbnail);
 
-//================================>
+    const caption = `
+🎧 *${randomVideo.title}*
+🆔 *ID*: ${randomVideo.videoId}
+👀 *Views*: ${randomVideo.views}
+⏰ *Uploaded At*: ${randomVideo.ago}
+👤 *Author*: ${randomVideo.author.name}
+
+🔗 [Watch on YouTube](${randomVideo.url})
+    `;
+
+    const template = `
+╭─🎵 *Music Search Results*
+│
+├ 🎧 *Title*: ${randomVideo.title}
+├ 🆔 *ID*: ${randomVideo.videoId}
+├ 👀 *Views*: ${randomVideo.views}
+├ ⏰ *Uploaded At*: ${randomVideo.ago}
+├ 👤 *Author*: ${randomVideo.author.name}
+│
+├─🔗 [Watch on YouTube](${randomVideo.url})
+│
+╰─────────⭑ ©vorterx
+    `;
+
+    vorterx.sendMessage(m.from, { image: thumbnail, caption: caption }, { quoted: m });
