151a887e62aab699abc76b463ebcae1319330b1c
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-28 13:52:06 +0200

    vorterx:

--- config.js
+++ config.js
@@ -1,24 +1,33 @@
 const fs = require('fs');
-require('dotenv').config()
+require('dotenv').config();
 const chalk = require('chalk');
 
-        global.botName = process.env.BOTNAME || 'AZTEC MD',
-        global.prefix = process.env.PREFIX || '.',
-        global.session = process.env.SESSION || 'add something', //replace this to any text
-        global.thumb = process.env.THUMB || 'https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg',
-        global.mongodb = process.env.MONGODB || 'mongodb+srv://mk:mk@new.vxtfxii.mongodb.net/?retryWrites=true&w=majority',
-        global.port = process.env.PORT || '2000',
-        global.caption = process.env.CAPTION || 'By Aztec MD',
-        global.mods = (process.env.MODS || '').split(',')
+let config = {
+  botName: process.env.BOTNAME || 'AZTEC MD',
+  prefix: process.env.PREFIX || '.',
+  owner_number: process.env.OWNER_NUMBER || '27686881509',   
+  session: process.env.SESSION || 'add something',
+  thumb: process.env.THUMB || 'https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg',
+  mongodb_urI: process.env.MONGODB_URI || 'mongodb+srv://mk:mk@new.vxtfxii.mongodb.net/?retryWrites=true&w=majority',//Replace this
+  caption: process.env.CAPTION || 'By Aztec MD',
+  mods: process.env.MODS ? process.env.MODS.split(',') : [],
+};
 
+module.exports = config;
 
-module.exports = {
-mongodb: global.mongodb,
-  };
-let file = require.resolve(__filename);
-fs.watchFile(file, () => {
-  fs.unwatchFile(file);
-  console.log(chalk.redBright(`Update'${__filename}'`));
-  delete require.cache[file];
-  require(file);
+const configFile = require.resolve(__filename);
+
+watchAndReloadConfig(configFile);
+
+function watchAndReloadConfig(filePath) {
+  fs.watchFile(filePath, () => {
+    console.log(chalk.redBright(`Configuration file '${filePath}' updated`));
+    delete require.cache[filePath];
+    config = require(filePath);
+  });
+}
+
+process.on('SIGINT', () => {
+  console.log(chalk.yellowBright('Gracefully shutting down...'));
+  process.exit();
 });
