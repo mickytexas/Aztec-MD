580be7eddb1f08e0664795fbcd4052771fa42306
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 19:29:54 +0200

    vorterx

--- commands/_userFullpp.js
+++ commands/_userFullpp.js
@@ -3,32 +3,43 @@ const Jimp = require('jimp');
 
 module.exports = {
   name: "fullpp",
-  desc: "Change your Photo Profile",
+  desc: "To set the user  full Profile picture",
   category: "Group",
-  async xstart(vorterx, m, { mime, quoted, xReact, text }) => {
-    
+  async xstart(vorterx, m, { mime, quoted, xReact, text }) {
+  
     if (!/image/.test(mime)) {
-      await xReact("🚫");
-      return vorterx.sendMessage( m.from,{text: `*Reply to an image to change the profile picture *`, }, { quoted: m }
-      );
-    }
+    await xReact("🚫");
+    return vorterx.sendMessage( m.from,{text: `*Reply to an image to change the profile picture*`,},{ quoted: m });}
+     
     await xReact("✔️");
+
     let quotedimage = await vorterx.downloadAndSaveMediaMessage(quoted);
-    const { preview, fullPicture } = await generatePP(quotedimage);
+    const fullPicture = await generateProfilePicture(quotedimage, 720);
+    const preview = await generateProfilePicture(quotedimage, 128);
 
     await vorterx.setProfilePic(fullPicture);
 
     fs.unlinkSync(quotedimage);
 
-    vorterx.sendMessage( m.from,{ image: {url: fullPicture, },caption: `\n*Group icon has been changed successfully*`,},{ quoted: m });
+    vorterx.sendMessage(m.from, { image: {url: fullPicture,},caption: `\n*✔️Profile picture  has been set successfully*`,},{ quoted: m } );
 
-      async function generatePP(imagePath) {
+      async function generateProfilePicture(imagePath, size) {
       const jimp = await Jimp.read(imagePath);
-      const resizedImage = await jimp.cover(720, 720).getBufferAsync(Jimp.MIME_JPEG);
-      const previewImage = await jimp.cover(128, 128).getBufferAsync(Jimp.MIME_JPEG);
-       return { fullPicture: resizedImage.toString('base64'),
-       preview: previewImage.toString('base64'),
-      };
+      const { width, height } = jimp.bitmap;
+
+      let croppedImage;
+      if (width > height) {
+        const x = (width - height) / 2;
+        croppedImage = jimp.crop(x, 0, height, height);
+      } else if (height > width) {
+        const y = (height - width) / 2;
+        croppedImage = jimp.crop(0, y, width, width);
+      } else {
+        croppedImage = jimp;
       }
-     }
-    };
+
+      const resizedImage = await croppedImage.resize(size, size).getBufferAsync(Jimp.MIME_JPEG);
+      return resizedImage.toString('base64');
+      }
+    }
+   };
