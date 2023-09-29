e66e13e4ed1eb1644c336d9ea7a18071157cb0a3
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 13:49:32 +0200

    vorterx

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ let config = {
   owner_number: process.env.OWNER_NUMBER || '27686881509',   
   session: process.env.SESSION || 'add something',
   thumb: process.env.THUMB || 'https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg',
-  mongodb_uri: process.env.MONGODB_URI || 'mongodb+srv://mk:mk@new.vxtfxii.mongodb.net/?retryWrites=true&w=majority',//Replace this
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://mk:mk@new.vxtfxii.mongodb.net/?retryWrites=true&w=majority',//Replace this
   caption: process.env.CAPTION || 'By Aztec MD',
   mods: process.env.MODS ? process.env.MODS.split(',') : [],
   neofetchOptions: {
