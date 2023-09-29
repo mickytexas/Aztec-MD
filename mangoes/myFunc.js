5560d405ad8571b0cd18f648ef02b8fba40b635c
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 17:50:32 +0200

    vorterx

--- mangoes/myFunc.js
+++ mangoes/myFunc.js
@@ -38,6 +38,26 @@ exports.formatp = sizeFormatter({
 function format(...args) {
 	return util.format(...args)
 }
+exports.TelegraPh = (Path) => {
+  return new Promise(async (resolve, reject) => {
+    if (!fs.existsSync(Path)) return reject(new Error("File not Found"));
+    try {
+      const form = new BodyForm();
+      form.append("file", fs.createReadStream(Path));
+      const data = await axios({
+        url: "https://telegra.ph/upload",
+        method: "POST",
+        headers: {
+          ...form.getHeaders(),
+        },
+        data: form,
+      });
+      return resolve("https://telegra.ph" + data.data[0].src);
+    } catch (err) {
+      return reject(new Error(String(err)));
+    }
+  });
+};
 
 exports.getBuffer = async (url) => {
   try {
