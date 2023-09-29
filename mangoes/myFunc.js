1aa7219286ccb5158ef6beb46ae20953c3249227
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 17:58:33 +0200

    vorterx

--- mangoes/myFunc.js
+++ mangoes/myFunc.js
@@ -17,6 +17,23 @@ exports.isNumber = (number) => {
   const int = parseInt(number);
   return typeof int === "number" && !isNaN(int);
 };
+exports.fetchUrl = async (url, options) => {
+  try {
+    options ? options : {};
+    const res = await axios({
+      method: "GET",
+      url: url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+      },
+      ...options,
+    });
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
 exports.generateProfilePicture = async (buffer) => {
 	const jimp = await jimp_1.read(buffer)
 	const min = jimp.getWidth()
