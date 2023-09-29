9fe3d84ed413fc04f2524ce2710ab3fd893dbf4f
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 08:13:16 +0200

    vorterx::

--- commands/alive.js
+++ commands/alive.js
@@ -1,57 +1,58 @@
-//===============
+==============
 
 // MDE WITH LUV BY DIEGOSON 
 
 //================
 
 const fs = require("fs");
-const BOTNAME = global.botname;
+const BOTNAME = global.botName;
 const PREFIX = global.prefix;
 require("../config");
 
 module.exports = {
   name: 'alive',
   category: 'General',
-  description: 'A command to test if the bot is online',
-  async xstart(vorterx, m, { args, xReact }) {
-    await xReact("💗");
+  description: 'Check if the bot is online',
+  async xstart(vorterx, message, { args, xReact }) {
+    await xReact("💙");
 
-    const aztec = fs.readFileSync("./lib/imogs.jpg");
-    const msg = `*Hey ${m.pushName}!* 👋\n\nWelcome to ${process.env.BOTNAME}! 🤖\n\nI am a WhatsApp user bot developed by Diegoson.\n\n✨ Let's explore the world of automation together!\n\n📌 *Prefix*: ${global.prefix}\n📌 *Version*: 3.0.0\n\nType ${global.prefix}menu for full command list.`;
-    //This const msg transport the msg  to template and template receive the msg which u will see
+    const image = fs.readFileSync("./lib/imogs.jpg");
+    const userName = m.pushName;
+    const botName = process.env.BOTNAME;
+    const version = require(__dirname + "/package.json").version;
 
     const template = `
-╭─💗 *Bot Status*
-│
-├ Hey ${m.pushName}! 👋
-├ Welcome to ${process.env.BOTNAME}! 🤖
-├ I am a WhatsApp user bot developed by Diegoson.
-├ ✨ Let's explore the world of automation together!
-├
-├ 📌 *Prefix*: ${global.prefix}
-├ 📌 *Version*: ${ require(__dirname + "/package.json").version}
-│
-├ Type ${global.prefix}menu for full command list.
-│
-╰──────────⭑ ©vorterx
+    ╭─💙 *Bot Status*
+    │
+    ├ Hey ${userName}! 👋
+    ├ Welcome to ${botName}! 🤖
+    ├ I am a WhatsApp user bot developed by Diegoson.
+    ├ ✨ Let's embark the world of automation together!
+    ├
+    ├ 📌 *Prefix*: ${PREFIX}
+    ├ 📌 *Version*: ${version}
+    │
+    ├ Type ${PREFIX}menu for the full command list.
+    │
+    ╰──────────⭑ ©vorterx
     `;
 
     const messageOptions = {
-      image: aztec,
+      image: image,
       caption: template,
       contextInfo: {
         externalAdReply: {
-          title: 'vorterx team',
-          body: 'Best to use',
-          thumbnail: aztec,
+          title: 'Powerd by Aztec',
+          body: 'Unlash your imagination',
+          thumbnail: image,
           mediaType: 1,
           mediaUrl: '',
-          sourceUrl: 'https://vorterx.com/',
+          sourceUrl: 'https://vorterx.com',
           ShowAdAttribution: true,
         },
       },
     };
 
-    await vorterx.sendMessage(m.from, messageOptions, { quoted: m });
+    await vorterx.sendMessage(message.from, messageOptions, { quoted: message });
   },
 };
