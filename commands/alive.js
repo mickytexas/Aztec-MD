a93dad136ad48fa2c632f6edcf4b940e8af35faf
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-29 07:56:30 +0200

    vorterx

--- commands/alive.js
+++ commands/alive.js
@@ -17,21 +17,21 @@ module.exports = {
     await xReact("💗");
 
     const aztec = fs.readFileSync("./lib/imogs.jpg");
-    const msg = `*Hey ${m.pushName}!* 👋\n\nWelcome to ${global.BOTNAME}! 🤖\n\nI am a WhatsApp user bot developed by Diegoson.\n\n✨ Let's explore the world of automation together!\n\n📌 *Prefix*: ${global.prefix}\n📌 *Version*: 3.0.0\n\nType ${global.prefix}menu to get the full command list.`;
+    const msg = `*Hey ${m.pushName}!* 👋\n\nWelcome to ${process.env.BOTNAME}! 🤖\n\nI am a WhatsApp user bot developed by Diegoson.\n\n✨ Let's explore the world of automation together!\n\n📌 *Prefix*: ${global.prefix}\n📌 *Version*: 3.0.0\n\nType ${global.prefix}menu for full command list.`;
     //This const msg transport the msg  to template and template receive the msg which u will see
 
     const template = `
 ╭─💗 *Bot Status*
 │
 ├ Hey ${m.pushName}! 👋
-├ Welcome to ${global.BOTNAME}! 🤖
+├ Welcome to ${process.env.BOTNAME}! 🤖
 ├ I am a WhatsApp user bot developed by Diegoson.
 ├ ✨ Let's explore the world of automation together!
 ├
 ├ 📌 *Prefix*: ${global.prefix}
 ├ 📌 *Version*: 3.0.0
 │
-├ Type ${global.prefix}menu to get the full command list.
+├ Type ${global.prefix}menu for full command list.
 │
 ╰──────────⭑ ©vorterx
     `;
