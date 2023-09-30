bcd50352b539781b3652d0d63f9db35ef831359a
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-30 18:14:38 +0200

    vorterx ::

--- commands/_guessing_number.js
+++ commands/_guessing_number.js
@@ -1,70 +1,96 @@
-/*
+//  @MADE BY @DIEGOSON 
 
-     CREATED BY DIEGOSON FENANDEZ
-     
-*/
-     
+//  @AZTEC MD WABOT
+
+//  @ENJOY  GAMING BOT
+
 module.exports = {
   name: "number-guessing",
   description: "This is a number guessing game",
   category: "games",
-  async xstart(vorterx, m, { xReact, args,text }) {
-  
-    if (args[0] === "start-number-guessing") {
-      const randomNumber = Math.floor(Math.random() * 100) + 1;
-      let timeout = false;
+  async xstart(vorterx, m, { xReact, args, text }) {
+   
+      if (args[0] === "start-number-guessing") {
+      let player1 = m.author;
+      let player2 = null;
 
-      if (!args[1]) {
+       if (!m.mentions.users.first()) {
         await xReact("⛔");
-        await m.reply("⚠️ Please provide a number as your guess.");
+        await m.reply("⚠️ Please tag another user to play the game with.");
         return;
-      }
+       }
 
-      const userGuess = parseInt(args[1]);
+       player2 = m.mentions.users.first();
+       await m.reply(`${player2}, ${player1} wants to play number guessing game with you! Please reply with \`!join\` to join.`);
 
-      if (isNaN(userGuess)) {
-        await xReact("⛔");
-        await m.reply("❌ Invalid guess, please provide a valid number.");
-        return;
-      }
+       const filter = (response) =>
+        response.content.toLowerCase() === "!join" &&
+        response.author.id === player2.id;
+       const collector = m.channel.createMessageCollector(filter, {
+        time: 60000,
+        max: 1,     
+      });
 
-      if (userGuess < 1 || userGuess > 10) {
-        await xReact("🙂");
-        await m.reply("❌ Your guess should be between 1 and 10.");
-        return;
-      }
+       collector.on("collect", async (joinedMessage) => {
+         const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-      let message = "❌ Sorry, that's not the right number. Try again!";
-      if (userGuess === randomNumber) {
-        message = "🎉 Congrats! You guessed the correct number!";
-      }
+         const player1Guess = Math.floor(Math.random() * 100) + 1;
+         let player2Guess = null;
 
-      const D3centX = `
-        ┏━━━━━━━━━━━━━━━━━┓
+         let message = "Nobody guessed the correct number!";
+
+         await m.reply(`✅ It's now your turn, ${player2}. Please reply with your guess (a number between 1 and 100) within 60 seconds.`);
+
+         const guessCollector = m.channel.createMessageCollector(
+          (response) => response.author.id === player2.id,
+          { time: 60000, max: 1 }
+         );
+
+         guessCollector.on("collect", (guessMessage) => {
+           player2Guess = parseInt(guessMessage.content);
+
+            if (player2Guess !== NaN && player2Guess >= 1 && player2Guess <= 100) {
+            guessCollector.stop();
+            if (player1Guess === randomNumber && player2Guess === randomNumber) {
+              message = `🎉 Congrats! Both ${player1} and ${player2} guessed the correct number!`;
+            } else if (player1Guess === randomNumber) {
+              message = `🎉 Congrats! ${player1} guessed the correct number!`;
+            } else if (player2Guess === randomNumber) {
+              message = `🎉 Congrats! ${player2} guessed the correct number!`;
+            }
+
+            const D3centX = `┏━━━━━━━━━━━━━━━━━┓
         ┃   Number Guessing Game   ┃
         ┣━━━━━━━━━━━━━━━━━┫
-        ┃   Your Guess: ${userGuess}   ┃
+        ┃   ${player1}'s Guess: ${player1Guess}   ┃
+        ┃   ${player2}'s Guess: ${player2Guess}   ┃
         ┃   Correct Number: ${randomNumber}   ┃
         ┣━━━━━━━━━━━━━━━━━┫
         ┃   ${message}   ┃
-        ┗━━━━━━━━━━━━━━━━━┛
-      `;
+        ┗━━━━━━━━━━━━━━━━━┛`;
 
-      await m.reply(D3centX);
-    } else if (args[0] === "start-number-guessing-number") {
-      await m.reply("✅ The game has started. Guess a number between 1 and 100.");
-    } else if (args[0] === "quit-number-guessing") {
-      await m.reply("ℹ️ You have quit the number guessing game bye.");
-    } else {
-      await m.reply("❌ Invalid command. Available commands: `start-number-guessing`, `start-number-guessing-number`, `quit-number-guessing`.");
-    }
+           m.reply(D3centX);
+          }
+        });
 
-    const timeoutDuration = 60000;
-    setTimeout(() => {
-      if (!timeout) {
-        timeout = true;
-        m.reply("⌛️ Sorry, you ran out of time. The game has timed out bye.");
+         guessCollector.on("end", (collected) => {
+          if (collected.size === 0) {
+          m.reply(`⌛️ ${player2} did not provide a guess in time. The game has timed out.`);
+         }
+        });
+      });
+
+      collector.on("end", (collected) => {
+      if (collected.size === 0) {
+      m.reply(`⌛️ ${player2} did not join the game. The game has timed out.`);
       }
-    }, timeoutDuration);
+      });
+    } else if (args[0] === "start-number-guessing-number") {
+      await m.reply("✅ The game will start once you tag another user to play with you.");
+    } else if (args[0] === "quit-number-guessing") {
+      await m.reply("ℹ️ You have quit the number guessing game. Goodbye!");
+    } else {
+      await m.reply("❌ Invalid command. Available commands: `start-number-guessing`, `start-number-guessing-number`, `join`, `quit-number-guessing`.");
+    }
    },
-  };
+  }; 
