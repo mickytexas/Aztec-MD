85c7e0e70e5ef069827f90639e131f707cfa5fa9
diegoson <110030270+D3centX@users.noreply.github.com>
2023-09-30 15:51:11 +0200

    vorterx

---
+++ commands/_auto_react.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "auto-react",
+  description: "Enable or disable auto-reactions",
+  category: "user",
+  async xstart(vorterx, m, { xReact, activeAutoReact, deactiveAutoReact }) {
+
+    const command = m.body.toLowerCase();
+    if (command === "active_auto react") {
+      activeAutoReact();
+      const reactions = ["👍", "❤️", "😄", "🔥", "🎉", "🌟", "👏", "😊", "🥳", "💯"];
+      const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
+      await toReact(randomReaction + " Auto-reactions enabled!");
+    } else if (command === "deactive_auto react") {
+      deactiveAutoReact();
+      const reactions = ["👎", "💔", "😔", "🙅‍♀️", "😢", "👻", "😞", "🚫", "😕", "😭"];
+      const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
+      await toReact(randomReaction + " Auto-reactions disabled!");
+    } else {
+      const reactions = ["❌", "⚠️", "🤔", "🙄", "🔍", "🤷‍♂️", "🤦‍♀️", "👀", "💭", "👉"];
+      const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
+      await toReact(randomReaction + " Invalid command. Please use `active_auto react` or `deactive_auto react`.");
+     }
+    },
+  };
