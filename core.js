a4a4737238ad99e9e843fc5c3a9a000a41972e78
diegoson <110030270+D3centX@users.noreply.github.com>
2023-10-01 10:36:27 +0200

    vorterx

--- core.js
+++ core.js
@@ -1,168 +1,168 @@
-require('dotenv').config();
-const express = require('express');
-const { default: AztecConnect, Browsers, DisconnectReason } = require('@whiskeysockets/baileys');
-const { Collection, MessageType } = require('discord.js');
-const contact = require('./mangoes/contact.js');
-const fs = require('fs');
-const config = require('./config');
+/*
+
+AZTEC WABOT V3.0.0
+
+MULTI AUTH STATE
+
+*/
+
+require('./lib/message/vorterx.js');
+require('./config');
+
+const app = require('express')();
+const {
+  default: AztecConnect,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  makeInMemoryStore,
+  Browsers,
+  useMultiFileAuthState
+} = require('@whiskeysockets/baileys');
+const { Boom } = require('@hapi/boom');
+const P = require('pino');
+const PORT = global.port;
+const PREFIX = global.prefix;
+const BOTNAME = global.botname;
 const { imageSync } = require('qr-image');
 const path = require('path');
 const { say } = require('cfonts');
-const P = require('pino');
-const chalk = require('chalk');
+const express = require("express");
+const config = require('./config');
+const vorterx = require('./lib/message/vorterx.js');
+const { QuickDB } = require('quick.db');
+const MONGODB = global.mongodb;
 const mongoose = require('mongoose');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const fs = require("fs");
+const { Collection } = require('discord.js');
+const chalk = require('chalk');
+const { remove } = require('fs-extra');
+const contact = require("./mangoes/contact.js");
+const MessageHandler = require('./lib/message/vorterx.js');
+const driver = new MongoDriver(process.env.MONGODB);
+const store = makeInMemoryStore({
+  logger: P().child({
+    level: 'silent',
+    stream: 'store'
+  })
 });
 
-async function connectToMongoose() {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✔️ Connected to MongoDB');
-  } catch (error) {
-    console.error('➖ Failed to connect to MongoDB:', error);
-    process.exit(1);
-  }
+ (!process.env.MONGODB) {
+  console.error('❌Error: Provide a MONGODB URL to continue the process');
+  process.exit(1);
 }
 
-const { MakeSession } = require("./lib/session");
-
-const sessionCredentialsPath = "./connects/creds.json";
+mongoose.connect(MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 async function startAztec() {
-  await connectToMongoose();
+  const { MakeSession } = require("./lib/session");
+  const { state, saveCreds } = await useMultiFileAuthState('/connects/creds.json');
 
   if (!fs.existsSync(sessionCredentialsPath)) {
     await MakeSession(config.session_id, sessionCredentialsPath);
     console.log("Version: " + require("./package.json").version);
   }
 
-  const sessionState = JSON.parse(fs.readFileSync(sessionCredentialsPath));
+  say("%c AZTEC MD",
+    "font-weight: bold;font-size: 50px;color: red;text-shadow: 3px 3px 0 rgb(217,31,38) ,6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) ,12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) ,18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); margin-bottom: 12px; padding: 5%");
 
   const vorterxInstance = AztecConnect({
-    logger: P({ level: 'silent' }),
+    logger: P({ level: "silent" }),
     printQRInTerminal: true,
-    browser: Browsers.macOS('Desktop'),
-    auth: sessionState,
+    browser: Browsers.macOS("Desktop"),
+    auth: state,
     qrTimeout: undefined,
+    version: (await fetchLatestBaileysVersion()).version,
   });
 
-  say(
-    "%c AZTEC MD",
-    "font-weight: bold;font-size: 50px;color: red;text-shadow: 3px 3px 0 rgb(217,31,38) ,6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) ,12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) ,18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); margin-bottom: 12px; padding: 5%"
-  );
+  console.log("[🚀AZTEC WABOT HAS STARTED TO LAUNCH]");
 
-  const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  });
-
-  const Contact = mongoose.model('Contact', contactSchema);
-
+  store.bind(vorterxInstance.ev);
   vorterxInstance.cmd = new Collection();
-  vorterxInstance.contactDB = Contact;
+  vorterxInstance.DB = new QuickDB({
+    driver
+  });
+  vorterxInstance.contactDB = vorterxInstance.DB.table('contacts');
   vorterxInstance.contact = contact;
 
-  readCommands();
-
   async function readCommands() {
-    const cmdDir = './commands';
-    fs.readdir(cmdDir, (err, files) => {
-      if (err) {
-        console.error('Error reading commands directory:', err);
-        return;
-      }
-
-      files
-        .filter((file) => file.endsWith('.js'))
-        .forEach((file) => {
-          const command = require(`${cmdDir}/${file}`);
-          vorterxInstance.cmd.set(command.name, command);
-        });
-    });
+    const cmdFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+    for (const file of cmdFiles) {
+      const command = require(`./commands/${file}`);
+      vorterxInstance.cmd.set(command.name, command);
+    }
   }
+
+  await readCommands();
+
+  vorterxInstance.ev.on('creds.update', saveCreds);
 
   vorterxInstance.ev.on('connection.update', async (update) => {
     const { connection, lastDisconnect } = update;
+
     if (update.qr) {
       console.log(`[${chalk.red('!')}]`, 'white');
       vorterxInstance.QR = imageSync(update.qr);
     }
-    if (connection === 'open') {
-      console.log('💗 You have successfully logged in to Aztec');
-      const max = `\`\`\`*👾AZTEC  BEEN CONNECTED👾*\n*VERSION* : ${
-        require(__dirname + '/package.json').version
-      }\n*BOTNAME* : ${process.env.BOTNAME}\`\`\``;
-      vorterxInstance.sendMessage(
-        process.env.OWNER_NUMBER + '@s.whatsapp.net',
-        max,
-        MessageType.text
-      );
+
+    if (connection === "open") {
+      console.log("💗 You have successfully logged in to Aztec");
+    }
+
+    if (connection === "close" && lastDisconnect === DisconnectReason.invalidSession) {
+      console.log("🔒 InvalidSession detected. Please reauthenticate.");
     }
   });
 
-  vorterxInstance.ev.on('connection.update', async (update) => {
-    const { connection, lastDisconnect } = update;
-    if (connection === 'close' && lastDisconnect !== DisconnectReason.intended) {
-      console.log('Aztec disconnected. Trying to reconnect...');
-      await vorterxInstance.restart();
+  vorterxInstance.ev.on('connection.update', (update) => {
+    if (update.connection === 'close') {
+      console.log('🔌 Connection closed. Reason:', update.lastDisconnect);
     }
+  });
+
+  vorterxInstance.ev.on('qr.update', (update) => {
+    console.log('📲 QR Code updated. Scan QR code to log in.');
+  });
+
+  vorterxInstance.ev.on('auth-state.update', (update) => {
+    console.log('🔐 Authentication state updated:', update);
   });
 
   vorterxInstance.ev.on('error', (error) => {
-    console.error('Aztec encountered an error:', error);
+    console.error('❌ Error:', error);
   });
 
-  vorterxInstance.ev.on('qr', (qr) => {
-    console.log(`[${chalk.red('!')}]`, 'white');
-    vorterxInstance.QR = imageSync(qr);
-  });
-
-  vorterxInstance.ev.on('chat-update', async (chatUpdate) => {
-    if (chatUpdate.isNewMessage) {
-   const message = chatUpdate.messages.all()[0];
-
-      if (!message.message || !message.message.conversation || !message.message.conversation.startsWith('/'))
-        return;
-
-      const commandName = message.message.conversation.split(' ')[0].substring(1);
-      const args = message.message.conversation.split(' ').slice(1);
-
-      const command = vorterxInstance.cmd.get(commandName);
-      if (!command) {
-        await vorterxInstance.reply(
-          message.key.remoteJID,
-          MessageType.text
-        );
-        return;
-      }
-
-      try {
-        await command.execute(vorterxInstance, message, args);
-      } catch (error) {
-        console.error(`Error executing command ${commandName}:`, error);
-        await vorterxInstance.reply(
-          message.key.remoteJID,
-          MessageType.text
-        );
-      }
+  vorterxInstance.ev.on('message.new', async (message) => {
+    try {
+      await MessageHandler(vorterxInstance, message);
+    } catch (error) {
+      console.error('❌ Error handling message:', error);
     }
   });
 
-  await vorterxInstance.ready();
+  app.use(express.json());
 
-  fs.writeFileSync(sessionCredentialsPath, JSON.stringify(vorterxInstance.base64EncodedAuthInfo(), null, '\t'));
+  app.post("/vorterx", async (req, res) => {
+    const message = req.body;
+    try {
+      await MessageHandler(vorterxInstance, message);
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error('❌ Error handling message:', error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  });
+
+  app.listen(PORT, () => {
+   console.log(`🚀 Vorterx API listening on port ${PORT}`);
+  });
 }
 
- startAztec().catch((error) => {
-  console.error('An error occurred while starting Aztec:', error);
+startAztec().catch((error) => {
+  console.error('❌ Error starting Aztec:', error);
   process.exit(1);
 });
