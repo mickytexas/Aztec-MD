d3184b5dda81b068c151d01f51c4b764b29cba95
diegoson <110030270+D3centX@users.noreply.github.com>
2023-10-03 23:30:07 +0200

    vorterx

--- commands/chess.js
+++ commands/chess.js
@@ -8,10 +8,11 @@ module.exports = {
   description: 'Start a game of chess',
   category: 'Games',
   async xstart(vorterx, m, { xReact, sendText }) {
-   
+
+    await xReact('😃');
     const gameCode = m.from;
     if (activeGames[gameCode]) {
-    vorterx.sendMessage(m.from, 'A game is already in progress. Please wait for the current game to finish.');
+    vorterx.sendMessage(m.from, { text: 'A game is already in progress. Please wait for the current game to finish.'});
     return;
     }
 
@@ -20,14 +21,12 @@ module.exports = {
     players: [],
     };
 
-    vorterx.sendMessage(m.from, 'Chess game started! Waiting for the second player to join...');
-    const response = await vorterx.waitForMessage(m.from, {
-    fromMe: false,
-    quotedMessage: m,
+    vorterx.sendMessage(m.from,{ text: 'Chess game started! Waiting for the second player to join•••'});
+    const response = await vorterx.waitForMessage(m.from, { fromMe: false, quotedMessage: m,
     });
 
     if (response.text.trim().toLowerCase() !== 'join') {
-    vorterx.sendMessage(m.from, 'Invalid command. Please reply with the command "join" to join the game.');
+    vorterx.sendMessage(m.from,{ text: 'Invalid command. Please reply with the command "join" to join the game●●●'});
     delete activeGames[gameCode];
     return;
     }
@@ -37,7 +36,7 @@ module.exports = {
     const ground = Chessground(document.getElementById('chessboard'));
     let isWhiteTurn = true;
 
-    vorterx.sendMessage(m.from, `Game started! ${secondPlayer} has joined. It's Red's turn. Enter your move (eg b2 b4), or type 'quit' to end the game:`);
+    vorterx.sendMessage(m.from, {text: `Game started! ${secondPlayer} has joined. It's Red's turn. Enter your move (eg b2 b4), or type 'quit' to end the game:`});
 
     activeGames[gameCode].players.push(m.from, secondPlayer);
     activeGames[gameCode].game = chess;
@@ -45,19 +44,17 @@ module.exports = {
     while (!chess.game_over()) {
     const currentPlayer = isWhiteTurn ? 'White' : 'Red';
 
-    const moveCommand = await vorterx.waitForMessage(m.from, {
-    fromMe: false,
-    quotedMessage: m,
+    const moveCommand = await vorterx.waitForMessage(m.from, {fromMe: false, quotedMessage: m,
     });
 
     if (moveCommand.sender.id !== activeGames[gameCode].players[isWhiteTurn ? 0 : 1]) {
-    vorterx.sendMessage(m.from, "It's not your turn. Please wait for your opponent to make a move.");
+    vorterx.sendMessage(m.from,{ text: "It's not your turn. Please wait for your opponent to make a move."});
     continue;
      }
 
     const moveText = moveCommand.text.trim();
     if (moveText === 'quit') {
-    vorterx.sendMessage(m.from, 'Game ended. Thank you for playing');
+    vorterx.sendMessage(m.from, {text: 'Game ended. Thank you for playing'});
     delete activeGames[gameCode];
     return;
     }
@@ -68,14 +65,14 @@ module.exports = {
     const fen = chess.fen();
     ground.set({ fen });
     isWhiteTurn = !isWhiteTurn;
-    vorterx.sendMessage(m.from, `Move played: ${moveText}`);
+    vorterx.sendMessage(m.from,{text: `Move played: ${moveText}`});
     } else {
-    vorterx.sendMessage(m.from, 'Invalid move, please try again.');
+    vorterx.sendMessage(m.from, {text: 'Invalid move, please try again.'});
     }
     }
 
     const result = chess.in_checkmate() ? 'Checkmate!' : 'Stalemate!';
-    vorterx.sendMessage(m.from, `Game over. ${result}`);
+    vorterx.sendMessage(m.from, {text: `Game over. ${result}`});
 
     delete activeGames[gameCode];
     },
@@ -86,10 +83,11 @@ module.exports = {
        description: 'Provides valid commands for chess',
        category: 'Games',
        async xstart(vorterx, m, { xReact }) {
-        vorterx.sendText(m.from, 'Valid commands for chess:\n\n' +
+       await xReact('⛔');
+       vorterx.sendText(m.from, {text: 'Valid commands for chess:\n\n' +
           '• To start a game: 🕹Use `chess-start`\n' +
           '• To make a move: 🕹Use `b2 b4`\n' +
-          '• To quit the game: 🕹Type `quit`');
+          '• To quit the game: 🕹Type `quit`'});
          },
         },
       ],
