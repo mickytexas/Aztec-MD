95331562f3dcf5756076b0959777f3b19ee9d419
diegoson <110030270+D3centX@users.noreply.github.com>
2023-10-03 18:58:19 +0200

    vorterx:

--- commands/_facebook.js
+++ commands/_facebook.js
@@ -5,24 +5,26 @@ module.exports = {
   alias: ['facebook'],
   description: 'To download facebook video',
   category: 'Downloads',
-  async xstart(vorterx, m,{ xReact, text, args }) {
-   
+  async xstart(vorterx, m, { xReact, text, args }) {
+    
     try {
-       const videoURL = args[0];
+      const videoURL = args[0];
 
-       if (!videoURL) { await xReact('⛔');
-        return await xReact.sendMessage(m.from,{text: 'Please provide a Facebook video URL'});
+      if (!videoURL) { await xReact('⛔');
+      await vorterx.sendMessage(m.from, 'Please provide a Facebook video URL');
+      return;
       }
-      await xReact('📤');
+
+      await xReact.sendText(m.from, '📤');
       await vorterx.sendMessage(m.from,
         `🔢 Reply below number to select video quality:\n\n` +
         `*1 | 720p (HD) VIDEO*\n\n` +
         `*2 | 360p (SD) VIDEO*\n\n` +
         `└───────────◉`
-      );
+       );
 
-      const response = await vorterx.waitForMessage(m.from, {fromMe: false,quotedMessage: m,mentions: true,
-      regex: /^[12]$/,
+       const response = await vorterx.waitForMessage(m.from, {fromMe: false,quotedMessage: m,mentions: true,
+        regex: /^[12]$/,
        });
 
         const selectedNumber = response.text.trim();
@@ -31,8 +33,9 @@ module.exports = {
         await downloader.downloadHD(videoURL) :
         await downloader.downloadSD(videoURL);
 
-      if (!downloadInfo.success) {
-        return await vorterx.sendMessage(m.from,{ text: `Failed to download the video. Reason: ${downloadInfo.error}`});
+       if (!downloadInfo.success) {
+       await vorterx.sendMessage(m.from, `Failed to download the video. Reason: ${downloadInfo.error}`);
+       return;
       }
 
       const DiegosonX = `[*FB DOWOAD*]\n` +
@@ -41,9 +44,10 @@ module.exports = {
         `🙂Views: ${downloadInfo.views}\n\n` +
         `└───────────◉`;
 
-     await vorterx.sendMessage(m.from, downloadInfo.videoPath,'video.mp4', DiegosonX);
-    } catch (error) {
-    await vorterx.sendMessage(m.from, 'An error occurred while downloading the video');
+      await vorterx.sendMessage(m.from,{file: downloadInfo.videoPath,filename: 'video.mp4',caption: DiegosonX}
+     );
+     } catch (error) {
+      await vorterx.sendMessage(m.from, 'An error occurred while downloading the video');
     }
    },
   };
