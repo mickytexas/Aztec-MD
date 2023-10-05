3054bc71dcfa054dff1273e890af220ab1d802f0
diegoson <110030270+D3centX@users.noreply.github.com>
2023-10-05 23:39:17 +0200

    vorterx

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ let config = {
   prefix: process.env.PREFIX || '.',
   owner_number: process.env.OWNER_NUMBER || '27686881509',   
   session_id: process.env.SESSION_ID || 'add something',
+  level_up: process.env.LEVEL_UP || '', //Set true or false or enable/ disable
   menu: process.env.MENU || '', //2 Is default menu aztec 0 is Suhail md menu 
   thumb: process.env.THUMB || 'https://i.ibb.co/C7TXRcH/photo-1678483789107-0029c61fdcca.jpg',
   mods: process.env.MODS ? process.env.MODS.split(',') : [],
