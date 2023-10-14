122d499d848ad95cb7d98a8b97d4fdf43231d9f5
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-10-14 14:34:50 +0200

    vorterx

---
+++ commands/_daily_slot.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+module.exports = {
+  name: "daily",
+  description: "daily gold for slot",
+  category: "economy",
+  async xstart(vorterx, m, { text, xReact, isGroup, args }) {
+    
+     if (!isGroup) {
+     await toReact("😎");
+     return vorterx.sendMessage(m.from,{ text: "*😐This command can only work in groups*" },{ quoted: m });
+     }
+     await xReact("🏙️");
+     let user = m.sender;
+     const cara = "cara";
+     const daily = { cd: false, cdL: "00:00:00", amount: 500 };
+     if (daily.cd) {
+      await vorterx.sendMessage( m.from,{image: fs.readFileSync("./lib/connect/daily.png"),caption: `\n『 😃 You already claimed your daily revenue, Come back in ${daily.cdL} to claim again 』`,},{ quoted: m });
+     } else {
+      return vorterx.sendMessage(m.from, { text: `『 🎉 You have claimed your daily revenue of ${daily.amount} 』` },{ quoted: m });
+     }
+     },
+     };
